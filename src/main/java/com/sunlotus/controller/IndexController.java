d23f403d7f362e4c06f508086d7e12e3ed82c64a
LXQ <LXQ@LXQ-PC>
2019-02-28 23:02:07 +0800

    你好宿舍

--- src/main/java/com/sunlotus/controller/IndexController.java
+++ src/main/java/com/sunlotus/controller/IndexController.java
@@ -1,13 +1,13 @@
 package com.sunlotus.controller;
 
-import java.util.List;
 
 import com.jfinal.core.Controller;
-import com.sunlotus.common.model.Opend_log;
-import com.sunlotus.common.model.TaskConfig;
 
 public class IndexController extends Controller {
 
+	/**
+	 * 主页
+	 */
 	public void index(){
 		/*List<Opend_log> opendlog = Opend_log.dao.find("SELECT * FROM opennumber_log ORDER BY create_time DESC LIMIT 200");
 		TaskConfig tc = TaskConfig.dao.findById(1);
@@ -20,7 +20,108 @@ public class IndexController extends Controller {
 		render("/page/front/index.html");
 	}
 	
+	/**
+	 * 开奖记录
+	 */
 	public void openlog(){
 		render("/page/front/log.html");
+	}
+	
+	/**
+	 * 成员
+	 */
+	public void members(){
+		render("/page/front/member.html");
+	}
+	
+	/**
+	 * 客服
+	 */
+	public void kefus(){
+		render("/page/front/kefu.html");
+	}
+	
+	/**
+	 * 设置
+	 */
+	public void setups(){
+		render("/page/front/setup.html");
+	}
+	
+	/**
+	 * 个人信息
+	 */ 
+	public void selfInfos(){
+		render("/page/front/selfInfo.html");
+	}
+	
+	/**
+	 * 上传二维码
+	 */
+	public void uperweimas(){
+		render("/page/front/uperweima.html");
+	}
+	
+	/**
+	 * 下级拉人
+	 */
+	public void xiajilarens(){
+		render("/page/front/xiajilaren.html");
+	}
+	
+	/**
+	 * 房主信息
+	 */
+	public void housers(){
+		render("/page/front/houser.html");
+	}
+	
+	/**
+	 * 充值记录
+	 */
+	public void rechs(){
+		render("/page/front/rech.html");
+	}
+	
+	/**
+	 * 退款记录
+	 */
+	public void refunds(){
+		render("/page/front/refund.html");
+	}
+	
+	/**
+	 * 交易明细
+	 */
+	public void transs(){
+		render("/page/front/trans.html");
+	}
+	
+	/**
+	 * 赔率查看
+	 */
+	public void oddss(){
+		render("/page/front/odds.html");
+	}
+	
+	/**
+	 * 下注查看
+	 */
+	public void betss(){
+		render("/page/front/bets.html");
+	}
+	
+	/**
+	 * 下级查看
+	 */
+	public void xiajisees(){
+		render("/page/front/xiajisee.html");
+	}
+	
+	/**
+	 * 玩法说明
+	 */
+	public void plays(){
+		render("/page/front/play.html");
 	}
 }
