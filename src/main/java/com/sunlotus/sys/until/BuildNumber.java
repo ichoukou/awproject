1a8902b285586d28701b6a73a60879d1f3e365d6
XXW <XXW@XXW-PC>
2019-03-27 23:30:02 +0800

    对方水电费

--- src/main/java/com/sunlotus/sys/until/BuildNumber.java
+++ src/main/java/com/sunlotus/sys/until/BuildNumber.java
@@ -82,12 +82,11 @@ public class BuildNumber {
 		    	ls.add(c+"");
 		    }
 		}
-		//System.out.println(ls);
 		return ls;
     }
 	
 	/**
-	 * 获取不重复的
+	 * 获取不同的组合（二字现）
 	 */
 	public static List<String> getNoRepetNum(String initNum){
 		String[] s = initNum.split(",");
@@ -115,11 +114,135 @@ public class BuildNumber {
 	}
 	
 	/**
-	 * 添加重复的
+	 * 获取不同的组合（三字现）（四个数字都没有重复）
+	 */
+	public static List<String> getNumMuch(String initNum){
+		String[] s = initNum.split(",");
+		List<String> lsj = new ArrayList<String>();
+		for(int i=0; i<4; i++){
+			for(int j=0; j<4; j++){
+				for(int k=0; k<4; k++){
+					boolean d = false;
+					String buNum = "";
+					if(!s[i].equals(s[j])&&!s[i].equals(s[k])&&!s[j].equals(s[k])){
+						buNum = s[i]+s[j]+s[k];
+					}
+					for(String ls : lsj){
+						if(ls.equals(buNum)){
+							d = true;
+							break;
+						}
+					}
+					if(!d&&!buNum.equals("")){
+						lsj.add(buNum);
+					}
+					
+				}
+			}
+		}
+		return lsj;
+	}
+	
+	/**
+	 * 获取不同的组合（三字现）（有一个数字重复）
+	 */
+	public static List<String> getNumMuch(String initNum, String reNum){
+		String[] s = initNum.split(",");
+		List<String> lsj = new ArrayList<String>();
+		for(int i=0; i<4; i++){
+			for(int j=0; j<4; j++){
+				for(int k=0; k<4; k++){
+					boolean d = false;
+					String buNum = "";
+					if(!s[i].equals(s[j])&&!s[i].equals(s[k])&&!s[j].equals(s[k])){
+						buNum = s[i]+s[j]+s[k];
+					}
+					for(String ls : lsj){
+						if(ls.equals(buNum)){
+							d = true;
+							break;
+						}
+					}
+					if(!d&&!buNum.equals("")){
+						lsj.add(buNum);
+					}
+					
+				}
+			}
+		}
+		
+		List<String> norepNum = new ArrayList<String>();
+		for(String str : s){
+			boolean isp = true;
+			for(String nor : norepNum){
+				if(str.equals(nor)){
+					isp = false;
+					break;
+				}
+			}
+			if(isp){
+				norepNum.add(str);
+			}
+		}
+		
+		//System.out.println(norepNum+"/"+reNum);
+		List<String> lsjs = new ArrayList<String>();
+		for(int i=0; i<3; i++){
+			for(int j=0; j<3; j++){
+				boolean d = false;
+				String buNum = "";
+				if(!s[i].equals(s[j])){
+					buNum = s[i]+s[j];
+				}
+				
+				for(String ls : lsjs){
+					if(ls.equals(buNum)){
+						d = true;
+						break;
+					}
+				}
+				if(!d&&!buNum.equals("")){
+					if(buNum.indexOf(reNum)>=0){
+						String newBuNum = buNum;
+						buNum = reNum+""+newBuNum;
+						lsjs.add(buNum);
+						buNum = newBuNum+""+reNum;
+						lsjs.add(buNum);
+					}
+				}
+			}
+		}
+		
+		//System.out.println(lsjs);
+		//去重 lsjs
+		List<String> newLsjs = new ArrayList<String>();
+		for(int i=0; i<lsjs.size(); i++){
+			boolean d = true;
+			for(int j=0; j<newLsjs.size(); j++){
+				if(newLsjs.get(j).equals(lsjs.get(i))){
+					d = false;
+					break;
+				}
+			}
+			if(d){
+				newLsjs.add(lsjs.get(i));
+			}
+		}
+		//System.out.println(newLsjs);	
+		//合并
+		for(String se:newLsjs){
+			lsj.add(se);
+		}
+		return lsj;
+	}
+	
+	
+	/**
+	 * 添加重复的（二字现）
 	 * @param opNum
 	 * @return
 	 */
-	public static List<String> AddRepetNum(String opNum){
+	public static List<String> GetErZiXian(String opNum){
 		String nos = opNum.replaceAll(",", "");
 		List<String> lstr = checkDifferentNot(nos);
 		List<String> lst = getNoRepetNum(opNum);
@@ -154,12 +277,40 @@ public class BuildNumber {
 		return lst;
 	}
 	
+	public static List<String> GetSanZiXian(String opNum){
+		List<String> lst = new ArrayList<String>();
+		String nos = opNum.replaceAll(",", "");
+		List<String> lstr = checkDifferentNot(nos);
+		if(lstr.size()==0){
+			lst = getNumMuch(opNum); //没有重复的【已解决】
+		}
+		if(lstr.size()==1){
+			lst = getNumMuch(opNum, lstr.get(0)); //有一个数重复的【已解决】
+		}
+		if(lstr.size()==2){
+			System.out.println(2);
+		}
+		if(lstr.size()==3){
+			System.out.println(3);
+		}
+		return lst;
+	}
+	
 	/**
 	 * 入口
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(AddRepetNum("0,4,6,2"));
+		//二字现全部出现中奖的号码
+		//System.out.println(GetErZiXian("0,4,6,2"));
+		//三字现全部出现中奖的号码
+		System.out.println(GetSanZiXian("6,5,2,5"));
+		/**
+		 *556 655 552 255 565 525 
+		 * 
+		 * 其实吧在茫茫人海中能遇到一个自己特别喜欢的人非常非常的难，所以我不想就这样错过你，这不只是上天给我的一次机会，而且也是给你的一次机会，防止你错过我这么温柔、善良、体贴、乐观的男生。哈哈哈
+		 * 
+		 */
 	}
 	
 }
