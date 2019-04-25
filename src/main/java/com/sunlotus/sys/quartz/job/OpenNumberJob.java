918758dfd4048cc0d5dd5c0019a86f3dc73d0e28
XXW <XXW@XXW-PC>
2019-04-25 22:33:15 +0800

    dffgdfg

--- src/main/java/com/sunlotus/sys/quartz/job/OpenNumberJob.java
+++ src/main/java/com/sunlotus/sys/quartz/job/OpenNumberJob.java
@@ -1,5 +1,6 @@
 package com.sunlotus.sys.quartz.job;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.quartz.Job;
@@ -8,11 +9,14 @@ import org.quartz.JobExecutionException;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 import com.sunlotus.common.model.OpenNumber;
 import com.sunlotus.common.model.Opend_log;
 import com.sunlotus.common.model.TaskConfig;
 import com.sunlotus.common.model.WechatUser;
 import com.sunlotus.common.model.XiazhuTable;
+import com.sunlotus.sys.until.BuildNumber;
 import com.sunlotus.wechat.until.HttpRequestUntil;
 
 public class OpenNumberJob implements Job{
@@ -41,9 +45,14 @@ public class OpenNumberJob implements Job{
 			JSONObject obj = jsonArray.getJSONObject(i);
 			Opend_log on = Opend_log.dao.findFirst("SELECT id FROM opennumber_log WHERE create_qihao = '"+obj.getString("expect")+"'");
 			if(null==on){ //如果这个为空，就说明还没开过的号码
+				String openNumStr = obj.getString("opencode");
+				String qihao = obj.getString("expect");
+				openNumStr = openNumStr.substring(1, openNumStr.length()-2);
+				openNumStr = "1"+openNumStr;
+				System.out.println("===================================开奖号码："+openNumStr);
 				Opend_log openNum = new Opend_log();
-				openNum.set("create_qihao", obj.getString("expect"));
-				openNum.set("create_open", obj.getString("opencode"));
+				openNum.set("create_qihao", qihao);
+				openNum.set("create_open", openNumStr);
 				openNum.set("create_time", obj.getString("opentime"));
 				boolean yessave = openNum.save();
 				if(yessave){
@@ -54,7 +63,119 @@ public class OpenNumberJob implements Job{
 					//开始统计中奖号码
 					//IsWin(obj.getString("expect"),obj.getString("opencode"));
 				}
+				
+				/**
+				 * 1、根据中奖的号码
+				 * 2、查找下注表里面是否有中奖的号码
+				 */
+				//计算二字现中奖
+				List<String> x1 = BuildNumber.GetErZiXian(openNumStr);
+				for(String s:x1){
+					List<XiazhuTable> xt = XiazhuTable.dao.find("SELECT * FROM xiazhu_table WHERE numType = 4 AND status = 0 AND number = ? AND qihao = ? ", s, qihao);
+					List<Record> lr = new ArrayList<Record>();
+					for(XiazhuTable winxt : xt){
+						Record wu = Db.findById("wechat_user", winxt.getStr("openId"));
+						double winMoney = winxt.getFloat("domoney")*9.7;
+						wu.set("balance", wu.getFloat("balance")+winMoney);
+						lr.add(wu);
+					}
+					Db.batchUpdate("wechat_user", lr, 200); //批量更新
+				}
+				//计算三字现中奖
+				List<String> x2 = BuildNumber.GetSanZiXian(openNumStr);
+				for(String s:x2){
+					List<XiazhuTable> xt = XiazhuTable.dao.find("SELECT * FROM xiazhu_table WHERE numType = 5 AND status = 0 AND number = ? AND qihao = ? ", s, qihao);
+					List<Record> lr = new ArrayList<Record>();
+					for(XiazhuTable winxt : xt){
+						Record wu = Db.findById("wechat_user", winxt.getStr("openId"));
+						double winMoney = winxt.getFloat("domoney")*46;
+						wu.set("balance", wu.getFloat("balance")+winMoney);
+						lr.add(wu);
+					}
+					Db.batchUpdate("wechat_user", lr, 200); //批量更新
+				}
+				//计算四字现中奖
+				List<String> x3 = BuildNumber.GetSiZiXian(openNumStr);
+				for(String s:x3){
+					List<XiazhuTable> xt = XiazhuTable.dao.find("SELECT * FROM xiazhu_table WHERE numType = 6 AND status = 0 AND number = ? AND qihao = ? ", s, qihao);
+					List<Record> lr = new ArrayList<Record>();
+					for(XiazhuTable winxt : xt){
+						Record wu = Db.findById("wechat_user", winxt.getStr("openId"));
+						double winMoney = winxt.getFloat("domoney")*360;
+						wu.set("balance", wu.getFloat("balance")+winMoney);
+						lr.add(wu);
+					}
+					Db.batchUpdate("wechat_user", lr, 200); //批量更新
+				}
+				//计算一字定中奖
+				List<String> d1 = BuildNumber.GetYiZiDing(openNumStr);
+				for(String s:d1){
+					List<XiazhuTable> xt = XiazhuTable.dao.find("SELECT * FROM xiazhu_table WHERE numType = 0 AND status = 0 AND number = ? AND qihao = ? ", s, qihao);
+					List<Record> lr = new ArrayList<Record>();
+					for(XiazhuTable winxt : xt){
+						Record wu = Db.findById("wechat_user", winxt.getStr("openId"));
+						double winMoney = winxt.getFloat("domoney")*9.7;
+						wu.set("balance", wu.getFloat("balance")+winMoney);
+						lr.add(wu);
+					}
+					Db.batchUpdate("wechat_user", lr, 200); //批量更新
+				}
+
+				//计算二字定中奖
+				List<String> d2 = BuildNumber.GetErZiDing(openNumStr);
+				for(String s:d2){
+					List<XiazhuTable> xt = XiazhuTable.dao.find("SELECT * FROM xiazhu_table WHERE numType = 1 AND status = 0 AND number = ? AND qihao = ? ", s, qihao);
+					List<Record> lr = new ArrayList<Record>();
+					for(XiazhuTable winxt : xt){
+						Record wu = Db.findById("wechat_user", winxt.getStr("openId"));
+						double winMoney = winxt.getFloat("domoney")*97;
+						wu.set("balance", wu.getFloat("balance")+winMoney);
+						lr.add(wu);
+					}
+					Db.batchUpdate("wechat_user", lr, 200); //批量更新
+				}
+
+				//计算三字定中奖
+				List<String> d3 = BuildNumber.GetSanZiDing(openNumStr);
+				for(String s:d3){
+					List<XiazhuTable> xt = XiazhuTable.dao.find("SELECT * FROM xiazhu_table WHERE numType = 1 AND status = 0 AND number = ? AND qihao = ? ", s, qihao);
+					List<Record> lr = new ArrayList<Record>();
+					for(XiazhuTable winxt : xt){
+						Record wu = Db.findById("wechat_user", winxt.getStr("openId"));
+						double winMoney = winxt.getFloat("domoney")*970;
+						wu.set("balance", wu.getFloat("balance")+winMoney);
+						lr.add(wu);
+					}
+					Db.batchUpdate("wechat_user", lr, 200); //批量更新
+				}
+
+				//计算四字定中奖
+				List<String> d4 = BuildNumber.GetSiZiDing(openNumStr);
+				for(String s:d4){
+					List<XiazhuTable> xt = XiazhuTable.dao.find("SELECT * FROM xiazhu_table WHERE numType = 1 AND status = 0 AND number = ? AND qihao = ? ", s, qihao);
+					List<Record> lr = new ArrayList<Record>();
+					for(XiazhuTable winxt : xt){
+						Record wu = Db.findById("wechat_user", winxt.getStr("openId"));
+						double winMoney = winxt.getFloat("domoney")*9700;
+						wu.set("balance", wu.getFloat("balance")+winMoney);
+						lr.add(wu);
+					}
+					Db.batchUpdate("wechat_user", lr, 200); //批量更新
+				}
+				//更新不中奖的人员的金额
+				List<Record> lose = Db.find("SELECT * FROM xiazhu_table WHERE status = 0 AND qihao = ?");
+				List<Record> loseUp = new ArrayList<Record>();
+				for(Record losr : lose){
+					Record wu = Db.findById("wechat_user", losr.getStr("openId"));
+					wu.set("balance", wu.getFloat("balance")-losr.getFloat("domoney"));
+					loseUp.add(wu);
+				}
+				Db.batchUpdate("wechat_user", loseUp, 200); //批量更新
+				//更新档期全部的下注号码为“已开奖”
+				Db.update("UPDATE xiazhu_table SET status = 1 WHERE status = 0 AND qihao = ?", qihao);
+				
 			}
+			
 		}
 	}
 	
